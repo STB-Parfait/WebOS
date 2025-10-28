b4d0f964967b44945172a0b07eeb1353747881a6
STB-Parfait <jeserchivekalt@gmail.com>
2025-10-28 14:37:24 -0300

    local database change

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
 
 const connectDb = async () => {
     try{
-        mongoose.connect(MONGO_URI)
+        const MONGO_URI = process.env.MONGO_URI;
+        await mongoose.connect(MONGO_URI)
         .then(() => console.log('Database connected'))
         .catch(err => console.log('ERROR: ',err));
     } catch(err){
