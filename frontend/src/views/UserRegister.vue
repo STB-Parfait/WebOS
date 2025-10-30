e817796191049dc785f216780f54e3353a4e6d3d
STB-Parfait <jeserchivekalt@gmail.com>
2025-10-30 14:01:40 -0300

    polish of UI style

--- frontend/src/views/UserRegister.vue
+++ frontend/src/views/UserRegister.vue
@@ -1,75 +1,111 @@
-<script>
+<script setup>
+
+    import { ref } from 'vue';
+
+    const activeTab = ref('none');
+
+    function toggleTab(tabName){
+        if(activeTab.value===tabName){
+            activeTab.value = 'none';
+        } else{
+            activeTab.value = tabName;
+        }
+    }
+
 </script>
 
 <template>
-    <div class="container">
-        <div class="card">
-            <img src="../../public/placeholder.svg" alt="placeholder.svg"/>
-            <h1>WebOS</h1>
-            <p>V. 0.0.1</p>
-        </div>
-        <div class="tab">
-            <input type="email" placeholder="email"/><br/>
-            <input type="password" placeholder="password"/>
-            <button>continue</button><br/>
-            <p>login</p>
-        </div>
+  <div class="container">
+    <div class="card">
+      <img src="/placeholder.svg" alt="placeholder.svg" />
+      <h1>WebOS</h1>
+      <p>V. 0.0.1</p>
     </div>
-    
+
+    <div class="tabs">
+      <button class="tab-btn" @click="toggleTab('login')">login</button>
+      <button class="tab-btn" @click="toggleTab('register')">register</button>
+    </div>
+
+    <div v-if="activeTab=='login'" class="tab login">
+      <input type="email" placeholder="email" /><br />
+      <input type="password" placeholder="password" />
+      <button>continue</button>
+    </div>
+
+    <div v-if="activeTab=='register'" class="tab register">
+      <input type="email" placeholder="email" /><br />
+      <input type="password" placeholder="password" /><br/>
+      <input type="password" placeholder="confirm password"/>
+      <button>continue</button>
+    </div>
+  </div>
 </template>
 
 <style>
-    .container{
-        width: fit-content;
-    }
-    .card{
-        width: fit-content;
-        padding: 15px;
-        background: linear-gradient(to bottom, #f5f5f5, #d5d5d5);
-        img{
-            width: 100px;
-            height: 100px;
-        }
-    }
-    h1{
-        margin-top: 15px;
-        margin-bottom: 5px;
-        font-family: "WDXL Lubrifont SC", sans-serif;
-        font-weight: 400;
-        font-style: normal;
-    }
-    p{
-        margin-top: 5px;
-        margin-bottom: 0px;
-        margin-left: 0px;
-        margin-right: 0px;
-        font-family: "WDXL Lubrifont SC", sans-serif;
-        font-weight: 400;
-        font-style: normal;
-    }
-    .tab{margin-top: 10px;
-        padding: 15px;
-        background: linear-gradient(to bottom, #f5f5f5, #d5d5d5);
-        clip-path: polygon(
-            0% 0%,        /* 1. Canto superior esquerdo */
-            100% 0%,       /* 2. Canto superior direito */
-            100% 70%,      /* 3. Borda direita até 80% */
-            30% 70%,       /* 4. Ponto de início da aba (45% da largura) */
-            23% 90%,      /* 5. Canto inferior da aba (40% da largura) */
-            0% 90%,       /* 6. Canto inferior esquerdo */
-            0% 80%         /* 7. Volta para a borda esquerda, na altura de 80% */
-        );
-        button{
-            display: in;
-        }
-        input{
-            margin-bottom: 5px;
-            margin-right: 5px;
-        }
-        p{
-            margin-top: 17px;
-            margin-bottom: 5px;
-            width: fit-content;
-        }
-    }
+.container {
+  width: fit-content;
+  position: relative;
+  font-family: "WDXL Lubrifont SC", sans-serif;
+}
+
+.card {
+  width: 261.883px;
+  padding: 15px;
+  background: linear-gradient(to bottom, #f5f5f5, #d5d5d5);
+  border: 1px solid #999;
+}
+
+.card img {
+  width: 100px;
+  height: 100px;
+}
+
+h1,
+p {
+  margin: 5px 0;
+}
+
+.tabs {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 8px;
+}
+
+.tab-btn {
+  flex: 1;
+  background: linear-gradient(to bottom, #f5f5f5, #d5d5d5);
+  border: 1px solid #999;
+  font-family: "WDXL Lubrifont SC", sans-serif;
+  font-size: 14px;
+  text-transform: lowercase;
+  cursor: pointer;
+  transition: 0.2s;
+  padding: 5px 0;
+}
+
+.tab-btn:hover {
+  background: linear-gradient(to bottom, #ffffff, #cfcfcf);
+}
+
+.tab-btn:active {
+  background: linear-gradient(to bottom, #d0d0d0, #aaaaaa);
+}
+
+.tab {
+    
+    margin-top: 10px;
+    padding: 15px;
+    background: linear-gradient(to bottom, #f5f5f5, #d5d5d5);
+    border: 1px solid #999;
+}
+
+.tab input {
+    margin-bottom: 5px;
+    width: 90%;
+}
+
+.tab button {
+  margin-top: 5px;
+}
 </style>
